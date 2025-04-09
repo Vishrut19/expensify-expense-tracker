4e402653802a76edeadcca56afdfa6b22d3e0846
rsrohitsingh511 <rsrohitsingh511@gmail.com>
2025-04-09 18:08:56 +0530

    Added Sign In and SIgn Up Screen i.e. Modal

---
+++ screens/SignInScreen.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import { View, Text, Image, TextInput, TouchableOpacity } from "react-native";
+import ScreenWrapper from "../components/screenWrapper";
+import { colors } from "../theme";
+import BackButton from "../components/backButton";
+import { useNavigation } from "@react-navigation/native";
+export default function SignInScreen() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigation = useNavigation();
+
+  const handleAddTrip = () => {
+    if (email && password) {
+      // Good to Gos
+      navigation.goBack(); //This is used to Dismiss the Modal or popup and then we are navigated to Home Screen
+      navigation.navigate("Home");
+    } else {
+      // Show Error
+    }
+  };
+
+  console.log(email, password);
+
+  return (
+    <ScreenWrapper>
+      <View className="flex justify-between h-full mx-4">
+        {/* View for Header/Top Bar */}
+        <View>
+          <View className="relative">
+            <View className="absolute top-0 left-0">
+              <BackButton />
+            </View>
+            <Text className={`${colors.heading} font-bold text-xl text-center`}>
+              Sign In
+            </Text>
+          </View>
+          {/* Banner Image */}
+          <View className="flex-row justify-center my-3 mt-5">
+            <Image
+              className="h-80 w-80"
+              source={require("../assets/images/login.png")}
+            />
+          </View>
+          {/* Form */}
+          <View className="space-y-3 mx-2">
+            <Text className={`${colors.heading} text-lg font-bold`}>Email</Text>
+            <TextInput
+              value={email}
+              placeholder="Enter Email"
+              onChangeText={(value) => setEmail(value)}
+              className="p-4 bg-white rounded-full mb-3"
+            />
+            <Text className={`${colors.heading} text-lg font-bold`}>
+              Password
+            </Text>
+            <TextInput
+              placeholder="Enter Password"
+              value={password}
+              secureTextEntry
+              onChangeText={(value) => setPassword(value)}
+              className="p-4 bg-white rounded-full mb-3"
+            />
+            <TouchableOpacity className="flex-row justify-end">
+              <Text>Forget Password ?</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View>
+          <TouchableOpacity
+            onPress={handleAddTrip}
+            style={{ backgroundColor: colors.button }}
+            className="my-6 rounded-full p-3 shadow-sm mx-2"
+          >
+            <Text className="text-center text-white font-bold text-lg">
+              Sign In
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </ScreenWrapper>
+  );
+}
---
+++ screens/WelcomeScreen.jsx
@@ -0,0 +1,46 @@
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import React from "react";
+import ScreenWrapper from "../components/screenWrapper";
+import { colors } from "../theme";
+import { useNavigation } from "@react-navigation/native";
+
+export default function WelcomeScreen() {
+  const navigation = useNavigation();
+  return (
+    <ScreenWrapper>
+      <View className="h-full flex justify-around">
+        <View className="flex-row justify-center mt-10">
+          <Image
+            source={require("../assets/images/welcome.gif")}
+            className="h-96 w-96 shadow"
+          />
+        </View>
+        <View className="mx-5 mb-20">
+          <Text
+            className={`text-center font-bold text-4xl ${colors.heading} mb-10`}
+          >
+            Expensify
+          </Text>
+          <TouchableOpacity
+            className="shadow p-3 rounded-full mb-5"
+            style={{ backgroundColor: colors.button }}
+            onPress={() => navigation.navigate("SignIn")}
+          >
+            <Text className="text-center text-white text-lg font-bold">
+              Sign In
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            className="shadow p-3 rounded-full"
+            style={{ backgroundColor: colors.button }}
+            onPress={() => navigation.navigate("SignUp")}
+          >
+            <Text className="text-center text-white text-lg font-bold">
+              Sign Up
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </ScreenWrapper>
+  );
+}
